b8a2a3e3dc79c6fdcac6ec87e70c8bf0b7e422a8
Pawel Barc <pawel.barc@laplateforme.io>
2024-05-16 22:22:20 +0200

    Le job 05 terminé

--- jour04/jobs/LaPlateforme/my-new-project/index.js
+++ jour04/jobs/LaPlateforme/my-new-project/index.js
@@ -15,6 +15,7 @@ async function run() {
         // Dostęp do bazy danych 'LaPlateforme'
         const database = client.db('LaPlateforme');
 
+        // Definicja schematu 'year' i dodanie danych
         const yearCollection = database.collection('year');
         const years = [
             { year: "Bachelor 1" },
@@ -23,22 +24,36 @@ async function run() {
         ];
         const yearInsertResult = await yearCollection.insertMany(years);
         console.log("Years added successfully!");
-        
+
+        // Pobranie ID dodanych lat studiów
         const yearIds = yearInsertResult.insertedIds;
 
-        // Definicja schematu 'student' i dodanie danych
+        // Definicja schematu 'student' i dodanie danych z powiązaniem z 'year'
         const studentCollection = database.collection('student');
         const students = [
-            { firstname: "Bob", lastname: "LeBricoleur", students_number: "2",yearIds: [0] },
-            { firstname: "John", lastname: "Doe", students_number: "3", yearIds: [1] },
-            { firstname: "Marine", lastname: "Dupont", students_number: "4", yearIds: [2] }
+            { firstname: "Bob", lastname: "LeBricoleur", students_number: "2", year_id: yearIds[0] },
+            { firstname: "John", lastname: "Doe", students_number: "3", year_id: yearIds[1] },
+            { firstname: "Marine", lastname: "Dupont", students_number: "4", year_id: yearIds[2] }
         ];
         await studentCollection.insertMany(students);
         console.log("Students added successfully!");
 
-        // Pobranie listy studentów
-        const studentList = await studentCollection.find().toArray();
-        return studentList; // Zwrócenie listy studentów
+        // Agregacja studentów z ich rokiem studiów
+        const studentWithYear = await studentCollection.aggregate([
+            {
+                $lookup: {
+                    from: 'year',
+                    localField: 'year_id',
+                    foreignField: '_id',
+                    as: 'cursus'
+                }
+            },
+            {
+                $unwind: '$cursus'
+            }
+        ]).toArray();
+
+        console.log("Students with their cursus:", studentWithYear);
 
     } catch (err) {
         console.error(err);
@@ -50,12 +65,4 @@ async function run() {
 }
 
 // Uruchomienie funkcji
-run()
-    .then(studentList => {
-        if (studentList !== undefined) {
-            console.log("List of students:", studentList); // Wyświetlenie listy studentów w konsoli
-        } else {
-            console.log("No students found."); // Wyświetlenie komunikatu, jeśli lista studentów jest undefined
-        }
-    })
-    .catch(console.error);
+run().catch(console.error);
