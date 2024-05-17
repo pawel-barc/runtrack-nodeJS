a82809dbca0224da00090bb4777afc75c63a600c
Pawel Barc <pawel.barc@laplateforme.io>
2024-05-17 12:27:46 +0200

    J'ai crée deux routes pour about et et page d'accueil

--- jour05/runtrack_express/projet_web/index.js
+++ jour05/runtrack_express/projet_web/index.js
@@ -1,11 +1,16 @@
 const express = require('express'); // Importation du module Express
 const app = express(); // Création d'une instance d'Express
 
-const port = 3000; // Définir le port sur lequel le serveur écoutera
+const port = 80; // Définir le port sur lequel le serveur écoutera
 
-// Définir une route pour l'URL de base ('/')
+// Route pour la page d'accueil ('/')
 app.get('/', (req, res) => {
-  res.send('Bienvenue sur notre site web!');
+  res.send('Bienvenue sur notre site web!'); // Opis strony głównej
+});
+
+// Route pour la page "À propos" ('/about')
+app.get('/about', (req, res) => {
+  res.send('À propos de notre projet'); // Opis strony "À propos"
 });
 
 // Démarrer le serveur et écouter sur le port défini
